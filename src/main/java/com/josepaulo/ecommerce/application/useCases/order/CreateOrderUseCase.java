2d90516143bf9ffc9474cdadbff93eb8ce7fd73c
José <josepaulo1607@gmail.com>
2025-07-31 12:21:58 -0300

    feat: subtract product stock on checkout

--- src/main/java/com/josepaulo/ecommerce/application/useCases/order/CreateOrderUseCase.java
+++ src/main/java/com/josepaulo/ecommerce/application/useCases/order/CreateOrderUseCase.java
@@ -4,6 +4,7 @@ import com.josepaulo.ecommerce.domain.entities.*;
 import com.josepaulo.ecommerce.domain.enums.OrderStatus;
 import com.josepaulo.ecommerce.domain.repositories.ICartRepository;
 import com.josepaulo.ecommerce.domain.repositories.IOrderRepository;
+import com.josepaulo.ecommerce.domain.repositories.IProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,7 @@ public class CreateOrderUseCase {
 
     private final ICartRepository cartRepository;
     private final IOrderRepository orderRepository;
+    private final IProductRepository productRepository;
 
     public OrderEntity checkout(Long userId) {
         CartEntity cart = cartRepository.findByUserId(userId)
@@ -24,6 +26,20 @@ public class CreateOrderUseCase {
         if (cart.getItems().isEmpty()) {
             throw new RuntimeException("Carrinho está vazio");
         }
+
+        for (CartItemEntity item : cart.getItems()) {
+            var product = item.getProduct();
+            if (product.getStock() < item.getQuantity()) {
+                throw new RuntimeException("Estoque insuficiente para o produto '" + product.getName() + "'");
+            }
+        }
+
+        cart.getItems().forEach(item -> {
+            var product = item.getProduct();
+            int novoEstoque = product.getStock() - item.getQuantity();
+            product.setStock(novoEstoque);
+            productRepository.save(product);
+        });
 
         List<OrderItemEntity> orderItems = cart.getItems().stream().map(item -> OrderItemEntity.builder()
                 .product(item.getProduct())
@@ -51,4 +67,5 @@ public class CreateOrderUseCase {
 
         return saved;
     }
+
 }
